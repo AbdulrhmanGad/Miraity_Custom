fb2f1e051ee130a64c243353a9f1e9a9f1c8c2b2
AbdulrhmanMohammed <a.abdeldaiem@dvit.me>
2020-02-08 10:26:27 +0200

    [ADD]  search customer  with name, phone, mobile , [ADD] search customer  orders and produts

---
+++ controllers/search_customer.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class Customer(http.Controller):
+
+    # this search for customer name, phone, mobile, email
+    # {"jsonrpc": "2.0","params":{"name":"1014527537"}}
+    # {"jsonrpc": "2.0","params":{"name":"Abdulrhman"}}
+    @http.route('/search/customer', type='json', auth='user')
+    def search_customer(self, **kw):
+        customer_ids = http.request.env['res.partner'].search(['|', '|', '|', ('name', 'ilike', kw['name']),
+                                                               ('phone', 'ilike', kw['name']),
+                                                               ('mobile', 'ilike', kw['name']),
+                                                               ('code', 'ilike', kw['name']),
+                                                               ])
+        customers = []
+        for customer in customer_ids:
+            customers.append({'name': customer.name})
+        return {'success': True, 'message': "Success", 'customers': customers}
+
+    # @http.route('/search/customer/orders', type='json', auth='user')
+    # def search_customer_orders(self, **kw):
+    #     sale_id = http.request.env['sale.order'].search([('name', 'ilike', kw['name'])])
+    #     products = []
+    #     for line in sale_id:
+    #         products.append({
+    #             'product': line.product_id.name,
+    #             'label': line.name,
+    #             'quantity': line.quantity,
+    #         })
+    #
+    #     return {'success': True, 'message': "Success", 'products': products}
+
+    @http.route('/search/order', type='json', auth='user')
+    def search_order(self, **kw):
+        customer_ids = http.request.env['res.partner'].search(['|', '|', '|', '|', ('name', 'ilike', kw['name']),
+                                                               ('phone', 'ilike', kw['name']),
+                                                               ('mobile', 'ilike', kw['name']),
+                                                               ('code', 'ilike', kw['name']),
+                                                               ('email', 'ilike', kw['name']),
+                                                               ])
+        orders = []
+        for customer in customer_ids:
+            sale_ids = http.request.env['sale.order'].search([('partner_id', '=', customer.id)])
+            order = []
+            for sale in sale_ids:
+                order.append({
+                    'order': sale.name,
+                    'date': sale.date_order,
+                    'shipping_no': sale.shipping_no,
+                    'status': sale.state,
+                })
+            orders.append({'customer': customer.name,
+                           'order': order})
+
+        return {'success': True, 'message': "Success", 'orders': orders}
+
+    # {"jsonrpc": "2.0","params":{"name":"S00005"}}
+    @http.route('/search/order/products', type='json', auth='user')
+    def search_order_products(self, **kw):
+        sale_id = http.request.env['sale.order'].search([('name', '=', kw['name'])])
+        order = []
+        for sale in sale_id.order_line:
+            order.append({
+                'product': sale.product_id.name,
+                'date': sale.order_id.date_order,
+                'shipping_no': sale.order_id.shipping_no,
+                'status': sale.order_id.state,
+            })
+
+        return {'success': True, 'message': "Success", 'order': sale_id.name, 'products': order}
