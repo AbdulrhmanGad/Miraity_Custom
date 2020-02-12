7d31ab775bd2703d98205d4ff9d32f66da671f6a
powerfulman86 <dba.ahmedmaher@gmail.com>
2020-02-12 18:20:13 +0200

    Merge branch 'master' of https://github.com/powerfulman86/Miraity_Custom
    # Conflicts:
    #	controllers/__pycache__/controllers.cpython-36.pyc
    #	models/__pycache__/__init__.cpython-36.pyc
    #	models/__pycache__/company.cpython-36.pyc
    #	models/__pycache__/contacts.cpython-36.pyc
    #	models/__pycache__/product_brand.cpython-36.pyc
    #	models/__pycache__/product_category.cpython-36.pyc
    #	models/__pycache__/settings.cpython-36.pyc
    #	models/contacts.py
    #	models/settings.py
    #	views/contacts.xml
    #	views/product_category.xml
    #	wizard/__pycache__/wizard.cpython-36.pyc

--- models/settings.py
+++ models/settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+import secrets
 
 
 class ResConfigSettings(models.TransientModel):
@@ -13,4 +14,15 @@ class ResConfigSettings(models.TransientModel):
                                         config_parameter='base_setup.account_receive_id', )
     warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string="Test Warehouse",
                                    config_parameter='base_setup.warehouse_id', default_model="sale.order")
+    sample_warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string="Test Warehouse",
+                                   config_parameter='base_setup.sample_warehouse_id', )
+    outsource_warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string="Test Warehouse",
+                                   config_parameter='base_setup.outsource_warehouse_id', )
     token_key = fields.Char(string="Token Key", config_parameter='base_setup.token', )
+    short_description = fields.Char(string="Short Description", config_parameter='base_setup.short_description', )
+    call_center_token = fields.Char(string="Call Center Token Key", config_parameter='base_setup.call_center_token', )
+
+    def generate_call_center_token(self):
+        for rec in self:
+            self.env['ir.config_parameter'].sudo().set_param('base_setup.call_center_token', secrets.token_hex(20))
+
