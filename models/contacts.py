d5274347fc5809334642ecc2a93f7eae58cf8043
AbdulrhmanMohammed <a.abdeldaiem@dvit.me>
2020-03-17 10:14:23 +0200

    Enhance

--- models/contacts.py
+++ models/contacts.py
@@ -65,10 +65,10 @@ class ResPartner(models.Model):
     def create(self, values):
         if self.env.user.company_id:
             sequence = self.env.user.company_id.partner_count
-            code2 = self.env['ir.sequence'].next_by_code('res.partner') or '/'
+            supplier_no = self.env['ir.sequence'].next_by_code('res.partner') or '/'
             seq = sequence + 1
             values['code'] = 'CT' + str(seq).zfill(4)
-            values['code2'] = str(code2)
+            values['supplier_no'] = str(supplier_no)
             self.env.user.company_id.partner_count += 1
         return super(ResPartner, self).create(values)
 
